f9701f3a8e425475883865699609a2e95965c077
IT21356704 <it21356704@my.sliit.lk>
2024-08-16 17:31:14 +0530

    Navigate between dynamic pages

--- my-first-next-app/src/app/products/vegis/[vegeid]/page.jsx
+++ my-first-next-app/src/app/products/vegis/[vegeid]/page.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 function page({params}) {
   return (
+    <>
     <div>Vege items {params.vegeid}</div>
+    <Link href='/products/vegis'>Back to Vegis</Link>
+    </>
   )
 }
 
