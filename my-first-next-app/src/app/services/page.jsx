b304faae2127869daaa018f37eabed3aa7ffa8e1
IT21356704 <it21356704@my.sliit.lk>
2024-08-16 16:47:46 +0530

    Navigate between pages

--- my-first-next-app/src/app/services/page.jsx
+++ my-first-next-app/src/app/services/page.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 function page() {
   return (
+    <>
     <div>Services Page</div>
+    <Link href="/">Back to home</Link>
+    </>
   )
 }
 
